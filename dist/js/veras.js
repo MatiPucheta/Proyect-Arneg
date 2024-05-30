04fbcb17737f3a21f82db3a3b5aadaf346e0fcc1
PucheDios <matiasp2004@gmail.com>
2024-05-30 19:47:33 -0300

    1. Modificación del archivo index.html: Dos buttons, el primero redireccionará al usario al archivo main.html, el segundo será a hacía un html en donde esté el veras. 2. Veras.html: Este archivo es donde se encontrará el Veras, además se contará con una barra lateral

---
+++ dist/js/veras.js
@@ -0,0 +1,54 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
+document.getElementById('option1').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar__1');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
+document.getElementById('option2').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar__2');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
+document.getElementById('option3').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar__3');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
+document.getElementById('option4').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar__4');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
+document.getElementById('option5').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar__5');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
