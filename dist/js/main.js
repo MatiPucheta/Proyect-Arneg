71ef8663eec65a5ea4b97a126307c4c0477a14d2
PucheDios <matiasp2004@gmail.com>
2024-05-30 21:49:17 -0300

    Agregado de paneles en main.html y función en main.js para la utilización de las teclas "flecha izquierda y derecha"

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,13 @@ function activate(e) {
 }
 
 document.addEventListener('click',activate,false);
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowRight') {
+    const items = document.querySelectorAll('.item');
+    slider.append(items[0]);
+  } else if (e.key === 'ArrowLeft') {
+    const items = document.querySelectorAll('.item');
+    slider.prepend(items[items.length - 1]);
+  }
+});
