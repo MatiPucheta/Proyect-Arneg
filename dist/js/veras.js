87aaccceed380515fed17861adace81e4c737b51
PucheDios <matiasp2004@gmail.com>
2024-05-31 13:54:20 -0300

    Imagenes de construcciones añadida. Además de mejora visual de los sidebar

--- dist/js/veras.js
+++ dist/js/veras.js
@@ -1,54 +1,55 @@
 document.getElementById('menu-icon').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    if (sidebar.style.right === '-310px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar.style.right = '0px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar.style.right = '-310px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
 
 document.getElementById('option1').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar__1');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    var sidebar__1 = document.getElementById('sidebar__1');
+    if (sidebar__1.style.right === '-620px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar__1.style.right = '310px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar__1.style.right = '-620px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
 
 document.getElementById('option2').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar__2');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    var sidebar__1 = document.getElementById('sidebar__2');
+    if (sidebar__1.style.right === '-620px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar__1.style.right = '310px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar__1.style.right = '-620px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
 
 document.getElementById('option3').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar__3');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    var sidebar__1 = document.getElementById('sidebar__3');
+    if (sidebar__1.style.right === '-620px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar__1.style.right = '310px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar__1.style.right = '-620px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
 
 document.getElementById('option4').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar__4');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    var sidebar__1 = document.getElementById('sidebar__4');
+    if (sidebar__1.style.right === '-620px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar__1.style.right = '310px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar__1.style.right = '-620px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
 
 document.getElementById('option5').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar__5');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
+    var sidebar__1 = document.getElementById('sidebar__5');
+    if (sidebar__1.style.right === '-620px') { /* Ajusta esto según el ancho de tu barra lateral */
+        sidebar__1.style.right = '310px';
     } else {
-        sidebar.style.display = 'none';
+      sidebar__1.style.right = '-620px'; /* Ajusta esto según el ancho de tu barra lateral */
     }
 });
+
 
